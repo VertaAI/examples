d43c18fb0887498c8a7721ef8c38115eba9ee6a3
Jared Stanbrough <98837737+jared-verta@users.noreply.github.com>
2022-07-13 15:53:01 -0700

    VRD-386 Promote self contained builds (#31)
    * feat: script for creating self contained builds of a model version
    
    * feat: use existing build for promotion, don't create endpoint
    
    * feat: warn about non self contained builds
    
    * fix: remove mentions of endpoints
    
    * fix: fix self contained flag
    
    * fix: promote by model version id and latest self contained build
    
    * fix: collapse build scan loop
    
    * fix: fix wrong return variable
    
    * fix: double check latest_date is not None

--- deployment/promotion/promote.py
+++ deployment/promotion/promote.py
@@ -1,33 +1,40 @@
 #!/usr/bin/env python3
 """
-This is a Python script that will copy/promote a Verta Endpoint from one environment
+This is a Python script that will copy/promote a Verta Build from one environment
 to another.
+
+- The script will use the model version passed in the VERTA_SOURCE_MODEL_VERSION_ID environment variable
+as the model version to promote.
+- The latest self contained build of the model version will be promoted. The promotion process will terminate if no
+self contained builds of the model version are found.
+- If you need to create a self contained build of a model version, use the create_scb.py script.
 
 Configuration is done via environment variables. All are mandatory:
 
-- VERTA_SOURCE_ENDPOINT_PATH: The endpoint path to promote from, ex. '/test'
+- VERTA_SOURCE_MODEL_VERSION_ID: The ID of the model version to promote
 - VERTA_SOURCE_HOST: The source Verta instance to promote from
 - VERTA_SOURCE_EMAIL: The email address for authentication to the source Verta instance
 - VERTA_SOURCE_DEV_KEY: The dev key associated to the email address on the source Verta instance
-- VERTA_SOURCE_WORKSPACE: The workspace associated with the endpoint on the source Verta instance
-- VERTA_DEST_ENDPOINT_PATH: The destination endpoint path, ex. '/test'
+- VERTA_SOURCE_WORKSPACE: The workspace associated with the build on the source Verta instance
 - VERTA_DEST_HOST: The destination Verta instance to promote to
 - VERTA_DEST_EMAIL: The email address for authentication to the destination Verta instance
 - VERTA_DEST_DEV_KEY: The dev key associated to the email address on the destination Verta instance
-- VERTA_DEST_WORKSPACE: The workspace associated with the endpoint on the destination Verta instance
+- VERTA_DEST_WORKSPACE: The workspace associated with the build on the destination Verta instance
 
 Optional environment variables to configure curl usage:
 VERTA_CURL_OPTS: Options to pass to curl. Defaults to '-O'
 
-With these values set, run the script. The script will not attempt to delete any data and will fail if the endpoint already exists in the destination.
+With these values set, run the script. The script will not attempt to delete any data and will fail if the model and version already exist in the destination.
 """
 
-import json
+from locale import atoi
+
 import requests
 import os
+import datetime
 
-env_vars = ['VERTA_SOURCE_ENDPOINT_PATH', 'VERTA_SOURCE_HOST', 'VERTA_SOURCE_EMAIL', 'VERTA_SOURCE_DEV_KEY',
-            'VERTA_SOURCE_WORKSPACE', 'VERTA_DEST_ENDPOINT_PATH', 'VERTA_DEST_HOST', 'VERTA_DEST_EMAIL', 'VERTA_DEST_DEV_KEY',
+env_vars = ['VERTA_SOURCE_MODEL_VERSION_ID', 'VERTA_SOURCE_HOST', 'VERTA_SOURCE_EMAIL', 'VERTA_SOURCE_DEV_KEY',
+            'VERTA_SOURCE_WORKSPACE', 'VERTA_DEST_HOST', 'VERTA_DEST_EMAIL', 'VERTA_DEST_DEV_KEY',
             'VERTA_DEST_WORKSPACE']
 params = {}
 
@@ -37,7 +44,6 @@ for param_name in env_vars:
         raise Exception("Missing environment variable %s", param_name)
     params[param_name] = param
 
-
 curl_opts = os.environ.get('VERTA_CURL_OPTS')
 if curl_opts:
     params['VERTA_CURL_OPTS'] = curl_opts
@@ -46,14 +52,13 @@ else:
 
 config = {
     'source': {
-        'endpoint_path': params['VERTA_SOURCE_ENDPOINT_PATH'],
+        'model_version_id': atoi(params['VERTA_SOURCE_MODEL_VERSION_ID']),
         'host': params['VERTA_SOURCE_HOST'],
         'email': params['VERTA_SOURCE_EMAIL'],
         'devkey': params['VERTA_SOURCE_DEV_KEY'],
         'workspace': params['VERTA_SOURCE_WORKSPACE']
     },
     'dest': {
-        'endpoint_path': params['VERTA_DEST_ENDPOINT_PATH'],
         'host': params['VERTA_DEST_HOST'],
         'email': params['VERTA_DEST_EMAIL'],
         'devkey': params['VERTA_DEST_DEV_KEY'],
@@ -64,7 +69,6 @@ config = {
 
 def copy_fields(fields, src, dest):
     for field in fields:
-        # Don't try to copy if nothing's there
         if field in src.keys():
             dest[field] = src[field]
 
@@ -124,19 +128,13 @@ def patch(auth, path, body):
     return res.json()
 
 
-def get_endpoint(auth, endpoint):
-    endpoints = post(auth, '/api/v1/deployment/endpoints', {"exactPaths": [endpoint]})
-    return endpoints['endpoints'][0]
-
-
-def get_endpoint_stages(auth, endpoint_id):
-    path = "/api/v1/deployment/workspace/{}/endpoints/{}/stages".format(auth['workspace'], endpoint_id)
-    stages = get(auth, path)
-    return stages['stages']
-
-
 def get_build(auth, build_id):
     path = "/api/v1/deployment/builds/{}".format(build_id)
+    return get(auth, path)
+
+
+def get_builds(auth, source):
+    path = "/api/v1/deployment/builds/?workspaceName={}&model_version_id={}".format(source['workspace'], source['model_version_id'])
     return get(auth, path)
 
 
@@ -146,10 +144,6 @@ def get_model_version(auth, model_version_id):
 
 def get_registered_model(auth, registered_model_id):
     return get(auth, '/api/v1/registry/registered_models/{}'.format(registered_model_id))['registered_model']
-
-
-def pp(js):
-    print(json.dumps(js, indent=4))
 
 
 def signed_artifact_url(auth, model_version_id, artifact):
@@ -181,7 +175,7 @@ def download_artifact(auth, model_version_id, artifact):
     os.system(curl_cmd)
 
 
-def download_artifacts(auth, model_version_id, artifacts):
+def download_artifacts(auth, model_version_id, artifacts, model_artifact):
     print("Downloading %d artifacts" % len(artifacts))
 
     downloaded_artifacts = []
@@ -192,8 +186,14 @@ def download_artifacts(auth, model_version_id, artifacts):
         }
         copy_fields(['artifact_type', 'key'], artifact, artifact_request)
         download_artifact(auth, model_version_id, artifact_request)
-
         downloaded_artifacts.append({'key': artifact['key'], 'artifact_type': artifact['artifact_type']})
+
+    model_artifact_request = {
+        'method': 'GET',
+        'model_version_id': model_version_id
+    }
+    copy_fields(['artifact_type', 'key'], model_artifact, model_artifact_request)
+    download_artifact(auth, model_version_id, model_artifact_request)
 
     return downloaded_artifacts
 
@@ -227,110 +227,80 @@ def upload_artifact(auth, model_version_id, artifact):
     return put_url
 
 
-def upload_artifacts(auth, model_version_id, promoted_model):
-    print("Uploading %d artifacts" % len(promoted_model['artifact_keys']))
-    promoted_model['uploaded_artifacts'] = {}
+def upload_artifacts(auth, model_version_id, artifacts):
+    print("Uploading %d artifacts" % len(artifacts))
+    uploaded_artifacts = {}
 
-    for artifact in promoted_model['artifact_keys']:
-        promoted_model['uploaded_artifacts'][artifact["key"]] = upload_artifact(auth, model_version_id, artifact)
+    for artifact in artifacts:
+        uploaded_artifacts[artifact["key"]] = upload_artifact(auth, model_version_id, artifact)
+    return uploaded_artifacts
 
 
-def fetch_promotion_data(_config):
+def get_promotion_data(_config):
     source = _config['source']
-    
-    print("Fetching promotion data for '%s'" % source['endpoint_path'])
+    model_version_id = source['model_version_id']
+
+    print("Fetching promotion data for model version %d" % source['model_version_id'])
     source_auth = auth_context(source['host'], source['email'], source['devkey'], source['workspace'])
-    endpoint = get_endpoint(source_auth, source['endpoint_path'])
-    stages = get_endpoint_stages(source_auth, endpoint['id'])
+    model_version = get_model_version(source_auth, model_version_id)
 
-    promotion_data = {
-        'endpoint': endpoint,
-        'stages': []
+    all_builds = get_builds(source_auth, source)
+
+    time_format = '%Y-%m-%dT%H:%M:%S.%fZ'
+
+    build = None
+    latest_date = None
+    for b in all_builds['builds']:
+        if 'self_contained' in b['creator_request']:
+            build_date = datetime.datetime.strptime(b['date_created'], time_format)
+            if not latest_date or build_date > latest_date:
+                latest_date = build_date
+                build = b
+
+    if not build or not latest_date:
+        print("No self contained builds found for model version id %d, promotion stopped." % source['model_version_id'])
+        raise SystemExit(1)
+
+    model = get_registered_model(source_auth,  model_version['registered_model_id'])
+    artifacts = download_artifacts(source_auth, model_version_id, model_version['artifacts'], model_version['model'])
+
+    promotion = {
+         'build': build,
+         'model_version': model_version,
+         'model': model,
+         'artifacts': artifacts
     }
-
-    for stage in stages:
-        cr = stage['creator_request']
-        promoted_stage = {'builds': []}
-        copy_fields(['name', 'enable_prediction_authz'], cr, promoted_stage)
-        promotion_data['stages'].append(promoted_stage)
-        components = stage['components']
-        for component in components:
-            build_id = component['build_id']
-            promoted_build = {'id': build_id}
-            promoted_stage['builds'].append(promoted_build)
-            build = get_build(source_auth, build_id)
-            location = build['location']
-            promoted_build['location'] = location
-
-            model_version_id = build['creator_request']['model_version_id']
-            promoted_model = {'model_version_id': model_version_id}
-            promoted_build['model'] = promoted_model
-            model_version = get_model_version(source_auth, model_version_id)
-
-            fields = ['environment', 'artifacts', 'attributes', 'registered_model_id', 'model', 'version',
-                      'readme_text']
-            copy_fields(fields, model_version, promoted_model)
-            registered_model_id = model_version['registered_model_id']
-            promoted_model['registered_model'] = get_registered_model(source_auth, registered_model_id)
-
-            artifacts_and_model = model_version['artifacts']
-            artifacts_and_model.append(model_version['model'])
-            promoted_model['artifact_keys'] = download_artifacts(source_auth, model_version_id,artifacts_and_model)
-    return promotion_data
+    return promotion
 
 
-def create_registered_model(auth, rmv):
-    print("Creating registered model '%s'" % rmv['version'])
+def create_model(auth, source_model, source_artifacts):
+    print("Creating registered model '%s'" % source_model['name'])
     path = '/api/v1/registry/workspaces/{}/registered_models'.format(auth['workspace'])
-    rm = rmv['registered_model']
-
-    model = {}
-    fields = ['labels', 'custom_permission', 'name', 'readme_text', 'resource_visibility']
-    copy_fields(fields, rm, model)
-    copy_fields(['artifacts', 'attributes'], rmv, model)
+    model = {
+        'artifacts': source_artifacts
+    }
+    copy_fields(['labels', 'custom_permission', 'name', 'readme_text', 'resource_visibility'], source_model, model)
     return post(auth, path, model)['registered_model']
 
 
-def create_registered_model_version(auth, registered_model, promoted_model):
-    print("Creating model version for model '%s'" % promoted_model['version'])
-    path = '/api/v1/registry/registered_models/{}/model_versions'.format(registered_model['id'])
+def create_model_version(auth, source_model_version, promoted_model):
+    print("Creating model version '%s'" % source_model_version['version'])
+    path = '/api/v1/registry/registered_models/{}/model_versions'.format(promoted_model['id'])
     model_version = {}
-    if 'labels' in registered_model.keys():
-        model_version['labels'] = registered_model['labels']
+    if 'labels' in source_model_version.keys():
+        model_version['labels'] = source_model_version['labels']
 
-    # model and artifacts omitted for patching after upload
-    fields = ['artifacts', 'attributes', 'environment', 'version', 'readme_text']
-    copy_fields(fields, promoted_model, model_version)
+    fields = ['artifacts', 'attributes', 'environment', 'version', 'readme_text', 'model']
+    copy_fields(fields, source_model_version, model_version)
     return post(auth, path, model_version)['model_version']
 
 
-def update_registered_model_version(auth, registered_model_id, model_version_id, model):
+def patch_model(auth, registered_model_id, model_version_id, model):
     print("Updating model artifact for model version '%s'" % model_version_id)
 
     path = '/api/v1/registry/registered_models/{}/model_versions/{}'.format(registered_model_id, model_version_id)
-    update = {'model': model['model']}
-    del update['model']['path']
-    return patch(auth, path, update)['model_version']
-
-
-def create_endpoint(auth, promoted_endpoint, dest_endpoint_path):
-    print("Creating endpoint '%s'" % dest_endpoint_path)
-    path = '/api/v1/deployment/workspace/{}/endpoints'.format(auth['workspace'])
-    endpoint = {}
-    copy_fields(['custom_permission', 'resource_visibility', 'visibility'], promoted_endpoint, endpoint)
-    endpoint['path'] = dest_endpoint_path
-    return post(auth, path, endpoint)
-
-
-def create_endpoint_stage(auth, endpoint, stage):
-    print("Creating endpoint stage '%s'" % stage['name'])
-
-    path = '/api/v1/deployment/workspace/{}/endpoints/{}/stages'.format(auth['workspace'], endpoint['id'])
-    endpoint_stage = {
-            'enable_prediction_authz': stage['enable_prediction_authz'],
-            'name': stage['name']
-        }
-    return post(auth, path, endpoint_stage)
+    update = {'model': model}
+    return patch(auth, path, update)
 
 
 def create_build(auth, model_version_id, external_location):
@@ -339,54 +309,47 @@ def create_build(auth, model_version_id, external_location):
     build = {
         'external_location': external_location,
         'model_version_id': int(model_version_id),
-        'self_contained': True,
     }
     return post(auth, path, build)
 
 
-def update_stage(auth, endpoint, stage, build):
-    print("Rolling out build '%d' to '%s'" % (build['id'], endpoint['creator_request']['path']))
-    path = '/api/v1/deployment/workspace/{}/endpoints/{}/stages/{}/update'.format(auth['workspace'], endpoint['id'],
-                                                                                  stage['id'])
-    update = {
-        "build_id": build['id'],
-        "strategy": "rollout"
-    }
-    ret = put(auth, path, update)
-    return ret
+# Modify this function to download and re-upload the build image as needed
+def upload_build(source_build):
+    source_location = source_build['location']
+
+    # Modify as appropriate
+    dest_location = source_location
+    print("Uploading from %s to %s" % (source_location, dest_location))
+    # This placeholder function simply returns the unmodified source location.
+    # Re-uploading the build image is only necessary if the source and destination environments use different S3 buckets
+    return dest_location
 
 
-def create_promoted_endpoint(_config, promotion_data):
-    source = _config['source']
+def create_promotion(_config, promotion):
     dest = _config['dest']
     
-    print("Starting promotion of '%s' to '%s'" % (source['endpoint_path'], dest['endpoint_path']))
     dest_auth = auth_context(dest['host'], dest['email'], dest['devkey'], dest['workspace'])
 
-    endpoint = create_endpoint(dest_auth, promotion_data['endpoint']['creator_request'], dest['endpoint_path'])
-    
+    print("Starting promotion")
+    build_location = upload_build(promotion['build'])
+    model = create_model(dest_auth, promotion['model'], promotion['artifacts'])
+    model_version = create_model_version(dest_auth, promotion['model_version'], model)
 
-    print("Created endpoint %d" % endpoint['id'])
+    artifacts_and_model = promotion['artifacts']
+    artifacts_and_model.append(model_version['model'])
+    artifact_paths = upload_artifacts(dest_auth, model_version['id'], artifacts_and_model)
 
-    for promoted_stage in promotion_data['stages']:
-        print("Promoting endpoint stage '%s'" % promoted_stage['name'])
-        stage = create_endpoint_stage(dest_auth, endpoint, promoted_stage)
-        
-        for promoted_build in promoted_stage['builds']:
-            print("Promoting source build id %d" % promoted_build['id'])
-            promoted_model = promoted_build['model']
-            registered_model = create_registered_model(dest_auth, promoted_model)
-            rmv = create_registered_model_version(dest_auth, registered_model, promoted_model)
-            upload_artifacts(dest_auth, rmv['id'], promoted_model)
-            update_registered_model_version(dest_auth, registered_model['id'], rmv['id'], promoted_model)
-            build = create_build(dest_auth, rmv['id'], promoted_build['location'])
-            update_stage(dest_auth, endpoint, stage, build)
+    model_artifact = model_version['model']
+    model_artifact['path'] = artifact_paths['model']
+    patch_model(dest_auth, model['id'], model_version['id'], model_artifact)
+
+    return create_build(dest_auth, model_version['id'], build_location)
 
 
 def promote(_config):
-    promotion_data = fetch_promotion_data(_config)
-    create_promoted_endpoint(_config, promotion_data)
-    print("Promotion complete")
+    promotion = get_promotion_data(_config)
+    promoted_build = create_promotion(_config, promotion)
+    print("Promoted build %d created." % promoted_build['id'])
 
 
 promote(config)
