fb8ee797e46c373ee4530ff16923a26afb8ffcf0
Heather MacDonald <80712961+hmacdonald-verta@users.noreply.github.com>
2022-08-25 09:58:47 -0700

    feat: [VRD-430] Update script to accept url and safety status (#32)

--- deployment/external-scan/save_external_scan_results.py
+++ deployment/external-scan/save_external_scan_results.py
@@ -7,14 +7,16 @@ from typing import List
 from verta import Client
 
 
-USAGE = f"Usage: python {sys.argv[0]} [--help] | build_id external_scan_results_file\n \
+USAGE = f"Usage: python {sys.argv[0]} [--help] | build_id url safety_status \n \
     \n\tbuild_id: the build with which to associate the results \
-    \n\texternal_scan_results_file: file with the results of the external scan"
+    \n\turl: url to scan results \
+    \n\tsafety_status: [safe, unsafe] whether the scan ultimately passed (safe) or failed (unsafe)"
 
 @dataclasses.dataclass
 class Arguments:
     build_id: int
-    external_scan_results_file: str
+    url: str
+    safety_status: str
 
 
 def validate(args: List[str]):
@@ -22,6 +24,8 @@ def validate(args: List[str]):
     if not args[0].isdigit():
         raise SystemExit("Type Error: build_id must be an int")
     args[0] = int(args[0])
+    if args[2] != "safe" and args[2] != "unsafe":
+        raise SystemExit("Type Error: safety_status must be 'safe' or 'unsafe'")
     try:
         arguments = Arguments(*args)
     except TypeError:
@@ -29,10 +33,10 @@ def validate(args: List[str]):
     return arguments
 
 
-def save_scan_results(build_id: int, scan_results: str, workspace_name: str):
+def save_scan_results(build_id: int, url: str, safety_status: str, workspace_name: str):
     print(f"saving scan results for build {build_id}")
     r = requests.put(f"https://{os.environ['VERTA_HOST']}/api/v1/deployment/workspace/{workspace_name}/builds/{build_id}/scan", \
-        data=f"{{ \"scan_status\": \"scanned\", \"scan_external_results\": \"{scan_results}\"}}", \
+        data=f"{{ \"scan_status\": \"scanned\", \"scan_external_results\": {{\"url\": \"{url}\", \"safety_status\": \"{safety_status}\"}}}}", \
         headers={'accept': 'application/json', 'Content-Type': 'application/json', 'Grpc-Metadata-source': 'PythonClient', 'Grpc-Metadata-email': f"{os.environ['VERTA_EMAIL']}", 'Grpc-Metadata-developer_key': f"{os.environ['VERTA_DEV_KEY']}"})
 
     if r.status_code != 200:
@@ -44,12 +48,7 @@ def save_scan_results(build_id: int, scan_results: str, workspace_name: str):
 def main(arguments: Arguments):
     client = Client()
     workspace_name = client.get_workspace()
-    f = open(arguments.external_scan_results_file)
-    # I'm bravely reading all at once because it better not be TOO much data or we won't be able to pass it to dep-api anyway
-    scan_results = f.read().strip()
-    save_scan_results(arguments.build_id, scan_results, workspace_name)
-
-    f.close()
+    save_scan_results(arguments.build_id, arguments.url, arguments.safety_status, workspace_name)
 
 
 if __name__ == "__main__":
