2b9532094419180fab7ec221de04800fa929c3d9
cfascina <cfascina@gmail.com>
2022-11-29 09:21:33 -0300

    Model processing images by URLs

---
+++ deployment/computer-vision/url/deploy.py
@@ -0,0 +1,151 @@
+import os
+import tempfile
+import tensorflow as tf
+import tensorflow_hub as hub
+import time
+import urllib.request
+
+from PIL import Image, ImageOps
+from verta import Client
+from verta.endpoint.autoscaling import Autoscaling
+from verta.endpoint.autoscaling.metrics import CpuUtilizationTarget, MemoryUtilizationTarget, RequestsPerWorkerTarget
+from verta.endpoint.resources import Resources
+from verta.endpoint.update import DirectUpdateStrategy
+from verta.environment import Python
+from verta.registry import VertaModelBase, verify_io
+from verta.utils import ModelAPI
+
+
+class DetectObject(VertaModelBase):
+    def __init__(self, artifacts = None):
+        module_handle = 'https://tfhub.dev/google/openimages_v4/ssd/mobilenet_v2/1'
+        self.detector = hub.load(module_handle).signatures['default']
+    
+    def handle_img(self, data, width = 512, height = 512):
+        _, path = tempfile.mkstemp(suffix = '.jpg')
+        file_path = f"{tempfile.gettempdir()}/tmp-{data[0].split('/')[-1]}"
+        urllib.request.urlretrieve(data[1], file_path)
+        img = Image.open(file_path)
+        img = ImageOps.grayscale(img)
+        img.thumbnail((width, height), Image.Resampling.LANCZOS)
+        img.save(path, format = 'JPEG', quality = 90)
+        
+        print(f"Image downloaded to {path}.")
+        return path
+
+    def load_img(self, path):
+        img = tf.io.read_file(path)
+        img = tf.image.decode_jpeg(img, channels = 3)
+        
+        return img
+
+    def filter_results(self, file, response, entity = 'Car', min_score = .2):
+        unused_keys = ['detection_class_labels', 'detection_class_names']
+        response = {key: value.numpy().tolist() for key, value in response.items()}
+        response = dict([(key, val) for key, val in response.items() if key not in unused_keys])
+        response['detection_class_entities'] = [v.decode() for v in response['detection_class_entities']]
+
+        entities = response['detection_class_entities']
+        scores = response['detection_scores']
+        bboxes = response['detection_boxes']
+        result = {}
+
+        for i in range(len(entities)):
+            if entities[i] == entity and scores[i] >= min_score:
+                ymin, xmin, ymax, xmax = bboxes[i]
+                result = {
+                    'file': file,
+                    'has_car': 1,
+                    'score': scores[i],
+                    'bboxes': {'ymin': ymin, 'xmin': xmin, 'ymax': ymax, 'xmax': xmax}
+                }
+                break
+
+        if len(result) == 0:
+            result = {
+                'file': file,
+                'has_car': 0,
+                'score': 0,
+                'bboxes': {'ymin': 0, 'xmin': 0, 'ymax': 0, 'xmax': 0}
+            }
+        
+        print(f"Found {result['has_car']} object(s).")
+        return result
+
+    def run_detector(self, file, image_path):
+        img = self.load_img(image_path)
+        img = tf.image.convert_image_dtype(img, tf.float32)[tf.newaxis, ...]
+        
+        response = self.detector(img)
+        result = self.filter_results(file, response)
+        
+        return result
+
+    def detect_objects(self, data):
+        start_time = time.time()
+        image_path = self.handle_img(data, 640, 480)
+        result = self.run_detector(data[0], image_path)
+        end_time = time.time()
+
+        print(f"Inference time: {end_time - start_time}.")
+        return result
+
+    @verify_io
+    def predict(self, data):
+        result = self.detect_objects(data)
+
+        return result
+
+PROJECT_NAME = 'Object Detection'
+os.environ['VERTA_HOST'] = ''
+os.environ['VERTA_EMAIL'] = ''
+os.environ['VERTA_DEV_KEY'] = ''
+
+client = Client(os.environ['VERTA_HOST'], use_git = False)
+project = client.set_project(PROJECT_NAME)
+registered_model = client.get_or_create_registered_model(
+    name = 'Object Detection', 
+    labels = ['object-detection']
+)
+
+input = {
+    "file_name": "",
+    "image_str": ""
+}
+output = {
+    "file_name": "",
+    "has_car": 0,
+    "score": 0,
+    "ymin": 0,
+    "xmin": 0,
+    "ymax": 0,
+    "xmax": 0
+}
+
+model = registered_model.create_standard_model(
+    model_cls = DetectObject,
+    environment = Python(requirements = ['tensorflow', 'tensorflow_hub', 'matplotlib']),
+    model_api = ModelAPI([input], [output]),
+    name = 'v1-url'
+)
+
+autoscaling = Autoscaling(min_replicas = 1, max_replicas = 20, min_scale = 0.1, max_scale = 10)
+autoscaling.add_metric(CpuUtilizationTarget(0.6))
+autoscaling.add_metric(MemoryUtilizationTarget(0.7))
+autoscaling.add_metric(RequestsPerWorkerTarget(1))
+resources = Resources(cpu = 2., memory = '12Gi')
+
+endpoint = client.get_or_create_endpoint('object-detection-v1-url')
+status = endpoint.update(
+    model, 
+    strategy = DirectUpdateStrategy(),
+    autoscaling = autoscaling,
+    resources = resources,
+    wait = True
+)
+
+assert status['status'] == "active"
+url = 'http://s3.amazonaws.com/verta-starter/street-view-images/000001_0.jpg'
+file = url.split('/')[-1]
+endpoint.get_deployed_model().predict([file, url])
+print(status)
---
+++ deployment/computer-vision/url/predict.py
@@ -0,0 +1,65 @@
+import multiprocessing as mp
+import os
+import pandas as pd
+import time
+
+from verta import Client
+
+
+def load_urls():
+    with open('urls.txt', 'r') as f:
+        return [[line.strip().split('/')[-1], line.strip()] for line in f]
+
+def show_metrics(n_urls, n_threads, start_time, end_time):
+    total_time = end_time - start_time
+    total_time = time.strftime('%Mm %Ss', time.gmtime(total_time))
+    
+    print(f'URLs processed: {n_urls}.')
+    print(f'Threads: {n_threads}.')
+    print(f'Total time: {total_time}.')
+
+def save_results(results, n_urls):
+    cols = list(results[0].keys())[:-1]
+    cols.extend(list(results[0]['bboxes'].keys()))
+    data = []
+
+    for item in results:
+        values = list(item.values())[0:3]
+        values.extend(list(item['bboxes'].values()))
+        data.append(values)
+
+    df = pd.DataFrame(data, columns = cols)
+    df.to_csv(f"{n_urls}.csv", index = False)
+
+def main():
+    os.environ['VERTA_HOST'] = ''
+    os.environ['VERTA_EMAIL'] = ''
+    os.environ['VERTA_DEV_KEY'] = ''
+
+    client = Client(os.environ['VERTA_HOST'], debug = True)
+    endpoint = client.get_or_create_endpoint('object-detection-v1-url')
+    model = endpoint.get_deployed_model()
+
+    n_urls = 10
+    n_threads = int(mp.cpu_count())
+    urls = load_urls()[:n_urls]
+
+    start_time = time.time()
+    pool = mp.Pool(n_threads)
+    map_results = pool.map_async(model.predict, urls, chunksize = 1)
+
+    while not map_results.ready():
+        print(f"URLs remaining: {map_results._number_left}")
+        time.sleep(1.5)
+
+    results = map_results.get()
+    pool.close()
+    pool.join()
+    end_time = time.time()
+
+    show_metrics(n_urls, n_threads, start_time, end_time)
+    save_results(results, n_urls)
+
+
+if __name__ == '__main__':
+    main()
