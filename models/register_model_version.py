0d1078e46d809d280f0df3aed98ed4652285b209
Michael Liu <miliu@verta.ai>
2023-04-27 14:54:34 -0700

    Use model filename as RMV name

--- models/register_model_version.py
+++ models/register_model_version.py
@@ -19,7 +19,8 @@ if __name__ == "__main__":
     for model_filename in MODEL_FILENAMES.splitlines():
         logger.info('Registering model "%s"', model_filename)
         model_ver = reg_model.create_standard_model(
-            pickle.load(model_filename),
+            name=os.path.splitext(model_filename)[0],
+            model_cls=pickle.load(model_filename),
             environment=Python.read_pip_file(
                 os.path.join(os.path.dirname(__file__), "requirements.txt"),
             ),
