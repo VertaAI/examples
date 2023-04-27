4ff7bd81d7720086e4e4d52f135aea9f524f5bc5
Michael Liu <miliu@verta.ai>
2023-04-27 14:07:00 -0700

    Register new models

--- models/register_model_version.py
+++ models/register_model_version.py
@@ -6,16 +6,17 @@ from verta import Client
 from verta.environment import Python
 
 REGISTERED_MODEL_NAME = os.environ["REGISTERED_MODEL_NAME"]
-MODEL_FILEPATH = os.environ["MODEL_FILEPATH"]
+MODEL_FILENAMES = os.environ["MODEL_FILENAMES"]
 
 
 if __name__ == "__main__":
     client = Client()
     reg_model = client.get_or_create_registered_model(REGISTERED_MODEL_NAME)
 
-    model_ver = reg_model.create_standard_model(
-        pickle.load(MODEL_FILEPATH),
-        environment=Python.read_pip_file(
-            os.path.join(os.path.dirname(__file__), "requirements.txt"),
-        ),
-    )
+    for model_filename in MODEL_FILENAMES.splitlines():
+        model_ver = reg_model.create_standard_model(
+            pickle.load(model_filename),
+            environment=Python.read_pip_file(
+                os.path.join(os.path.dirname(__file__), "requirements.txt"),
+            ),
+        )
